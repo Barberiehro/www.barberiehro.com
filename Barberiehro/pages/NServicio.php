51757b77e173854a2e1d6443ee840b3df6ce3e4a
Barberihro <davidbejaraguilar@gmail.com>
2017-05-19 15:38:37 -0500

    Jesus Aviles Gutierrez

---
+++ Barberiehro/pages/NServicio.php
@@ -0,0 +1,38 @@
+<div style="margin-top: 90px; margin-bottom: 450px; margin-left: 50px; ">
+	
+
+<div class="container">
+            <img src="img/registroser/registro1.png" style="margin-left: 220px">
+            <div class="col-md-6 col-md-offset-3 wow fadeInUp" data-wow-delay=".3s">
+
+                <form method="post" action="pages/registro.php">
+
+                    <div class="form-group">
+                        <input type="text" name="tipo1" class="form-control" required placeholder="Tipo">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="costo1" class="form-control" required placeholder="Costo">
+                    </div>
+
+                    <div class="form-group">
+                        <textarea class="form-control" name="descripcion1" rows="3" required placeholder="Descripción"></textarea>
+                    </div>
+
+                    <!-- <a href="#" class="btn-block" type="submit" name="submit"> <center>Registrar</center></a> -->
+
+                    <input class="btn btn-block" type="submit" name="submit" value="Registrar"/>
+
+                </form>
+
+            </div>
+
+
+        </div>
+</div>
+
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<div id="map3" style="margin-left: 500px; margin-top: 450px; margin-bottom: 100px; border-radius: 150px;" >
+</div>
+
+
